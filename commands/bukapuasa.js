5be68cf72eefbf377b2a944e127973b29da654f0
Akbar Maliki Haqoni Jati <akbar.maliki@ui.ac.id>
2023-08-28 14:49:05 +0700

    fix bug and add docker config

--- commands/bukapuasa.js
+++ commands/bukapuasa.js
@@ -14,16 +14,16 @@ const { SlashCommandBuilder } = require('discord.js');
 
 const coordinates = new Coordinates(-6.200000, 106.816666);
 const params = CalculationMethod.MoonsightingCommittee();
-const date = new Date();
-const prayerTimes = new PrayerTimes(coordinates, date, params);
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('bukapuasa')
-        .setDescription('Provides information about buka puasa or azan magrib in jakarta'),
+    .setName('bukapuasa')
+    .setDescription('Provides information about buka puasa or azan magrib in jakarta'),
     async execute(interaction) {
         // interaction.user is the object representing the User who ran the command
         // interaction.member is the GuildMember object, which represents the user in the specific guild
+        const date = new Date();
+        const prayerTimes = new PrayerTimes(coordinates, date, params);
         await interaction.reply("Waktu buka puasa di Jakarta hari ini adalah jam: "+moment(prayerTimes.maghrib).tz('Asia/Jakarta').format('h:mm z'));
     }
 }
